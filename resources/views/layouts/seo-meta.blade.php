a2f7743b69d9d02f30f468ba08b6a84c441116dd
Oyedokun Kehinde <oyedokunken@gmail.com>
2026-03-20 22:08:24 +0100

    Fix Laravel Routing Errors, .htacess Errors, Correct Page Routing

--- resources/views/layouts/seo-meta.blade.php
+++ resources/views/layouts/seo-meta.blade.php
@@ -115,7 +115,7 @@
     </script>
 @endif
 
- Additional Meta Tags --}}
+{{-- Additional Meta Tags --}}
 <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
 <meta name="googlebot" content="index, follow">
 <meta name="bingbot" content="index, follow">
